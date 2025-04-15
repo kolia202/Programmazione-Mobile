bea5ab9616075dc70475344aa528bba0d7c88b87
kolia202 <kolia.zanni@gmail.com>
2025-04-15 13:43:52 +0200

    test

--- app/src/main/java/com/example/byteme/MainActivity.kt
+++ app/src/main/java/com/example/byteme/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Helloooo $name!",
+        text = "Helloouoo $name!",
         modifier = modifier
     )
 }
