48262098cee137c43b8f46772822cc4c8dfd479c
kolia202 <kolia.zanni@gmail.com>
2025-04-27 12:01:38 +0200

    add font

--- app/src/main/java/com/example/byteme/ui/theme/Type.kt
+++ app/src/main/java/com/example/byteme/ui/theme/Type.kt
@@ -2,33 +2,62 @@ package com.example.byteme.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.byteme.R
 
+val poppinsFontFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
+    Font(R.font.poppins_blackitalic, FontWeight.Black),
+    Font(R.font.poppins_bolditalic, FontWeight.Bold),
+    Font(R.font.poppins_extrabolditalic, FontWeight.ExtraBold),
+    Font(R.font.poppins_extralightitalic, FontWeight.ExtraLight),
+    Font(R.font.poppins_italic, FontWeight.Normal),
+    Font(R.font.poppins_lightitalic, FontWeight.Light),
+    Font(R.font.poppins_mediumitalic, FontWeight.Medium),
+    Font(R.font.poppins_semibolditalic, FontWeight.SemiBold),
+    Font(R.font.poppins_thinitalic, FontWeight.Thin),
+    Font(R.font.poppins_blackitalic, FontWeight.Black)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
     )
-    */
 )
